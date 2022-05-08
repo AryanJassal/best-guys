f8bbaf336876867a2c185a71f02dea5fef775bcd
Aryan Jassal <aryanjassal.au@gmail.com>
2022-05-08 21:50:21 +1000

    added basic framework to store template

--- website/settings.py
+++ website/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
 
     # custom apps
-    'core'
+    'core',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'website.urls'
