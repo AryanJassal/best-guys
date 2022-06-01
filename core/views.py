5dd96eb1bff071e17d47122dbdf75779cb53f003
Aryan Jassal <aryanjassal.au@gmail.com>
2022-06-01 22:39:59 +1000

    cleaned and restructured code for static pages

--- core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('The url setup worked perfectly! <a href="store/product/hyperspectral-tv">Go to product.</a>')
-
--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # path('', views.home, name='home'),
-    path('product/hyperspectral-tv/', views.hyperspectral_tv, name='hyperspectral_tv'),
-    path('product/glass-s/', views.glass_s, name='glass_s'),
-]
